6a3aae2b139c06a9661152474ec23d7c866770a3
Xkevd <kevinalex11@hotmail.com>
2023-03-20 00:13:16 -0300

    changes

---
+++ src/js/events.js
@@ -0,0 +1,12 @@
+/* function getJSON(){
+  fetch('..json/eventsdb.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const objData = JSON.parse(data)
+    console.log(objData)
+  })
+}
+getJSON() */
+console.log('hello world');
